b55af8a5f35079efc82e229f8d0ab70bc63a6041
DynastyKids(づ｡◕‿‿◕｡)づ <shabibox@outlook.com>
2022-04-12 23:42:19 +1000

    Adding back E10 for WA, adding NT fuel method info

--- src/Controller/FuelController.php
+++ src/Controller/FuelController.php
@@ -24,7 +24,7 @@ class FuelController extends AppController
         $this->autoRender=false;
         $nswresults = TableRegistry::getTableLocator()->get('Nswfuel')->find()->select(['brand','name','address','loc_lat','loc_lng','U91','E10','P95','P98','DL','PDL','LPG','E85','B20']);
         $tasresults = TableRegistry::getTableLocator()->get('Tasfuel')->find()->select(['brand','name','address','loc_lat','loc_lng','U91','E10','P95','P98','DL','PDL','LPG','E85']);
-        $waresults = TableRegistry::getTableLocator()->get('Wafuel')->find()->select(['brand','name','address','loc_lat','loc_lng','U91','P95','P98','DL','PDL','LPG','E85']);
+        $waresults = TableRegistry::getTableLocator()->get('Wafuel')->find()->select(['brand','name','address','loc_lat','loc_lng','U91','P95','P98','DL','PDL','LPG','E85','LAF']);
         $ntresults = TableRegistry::getTableLocator()->get('Ntfuel')->find()->select(['brand','name','address','suburb','state','loc_lat','loc_lng','U91','E10','P95','P98','DL','PDL','LPG','E85','B20','LAF']);
         $qldresults = TableRegistry::getTableLocator()->get('Qldfuel')->find()->select(['brand','name','address','suburb','state','loc_lat','loc_lng','U91','E10','P95','P98','DL','PDL','LPG','E85','B20','LAF']);
         $saresults = TableRegistry::getTableLocator()->get('Safuel')->find()->select(['brand','name','address','suburb','state','loc_lat','loc_lng','U91','E10','P95','P98','DL','PDL','LPG','E85','B20','LAF']);
@@ -53,11 +53,13 @@ class FuelController extends AppController
         $waTable = TableRegistry::getTableLocator()->get('Wafuel')->find();
         if ($waTable->find()->count()>0) {
             $WaCheapU91 = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'U91'])->where(['U91 IS NOT NULL'])->orderAsc('U91')->limit(5)->toArray();
+            $WACheapE10 = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'E10'])->where(['E10 IS NOT NULL'])->orderAsc('E10')->limit(5)->toArray();
             $WaCheapP95 = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'P95'])->where(['P95 IS NOT NULL'])->orderAsc('P95')->limit(5)->toArray();
             $WaCheapP98 = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'P98'])->where(['P98 IS NOT NULL'])->orderAsc('P98')->limit(5)->toArray();
             $WaCheapLPG = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'LPG'])->where(['LPG IS NOT NULL'])->orderAsc('LPG')->limit(5)->toArray();
             $WaCheapDL = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'DL'])->where(['DL IS NOT NULL'])->orderAsc('DL')->limit(5)->toArray();
             $WaCheapPDL = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'PDL'])->where(['PDL IS NOT NULL'])->orderAsc('PDL')->limit(5)->toArray();
+            $WaCheapPDL = $waTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'LAF'])->where(['LAF IS NOT NULL'])->orderAsc('LAF')->limit(5)->toArray();
         }
         $tasTable=TableRegistry::getTableLocator()->get('Tasfuel')->find();
         if($tasTable->find()->count()>0) {
@@ -70,8 +72,21 @@ class FuelController extends AppController
             $TasCheapPDL = $tasTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'PDL'])->where(['PDL IS NOT NULL'])->orderAsc('PDL')->limit(3)->toArray();
         }
 
+        $ntTable=TableRegistry::getTableLocator()->get('Ntfuel')->find();
+        if($ntTable->find()->count()>0) {
+            $NtCheapU91 = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'U91'])->where(['U91 IS NOT NULL'])->orderAsc('U91')->limit(3)->toArray();
+            $NtCheapE10 = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'E10'])->where(['E10 IS NOT NULL'])->orderAsc('E10')->limit(3)->toArray();
+            $NtCheapP95 = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'P95'])->where(['P95 IS NOT NULL'])->orderAsc('P95')->limit(3)->toArray();
+            $NtCheapP98 = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'P98'])->where(['P98 IS NOT NULL'])->orderAsc('P98')->limit(3)->toArray();
+            $NtCheapLAF = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'LAF'])->where(['LAF IS NOT NULL'])->orderAsc('LAF')->limit(3)->toArray();
+            $NtCheapLPG = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'LPG'])->where(['LPG IS NOT NULL'])->orderAsc('LPG')->limit(3)->toArray();
+            $NtCheapDL = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'DL'])->where(['DL IS NOT NULL'])->orderAsc('DL')->limit(3)->toArray();
+            $NtCheapPDL = $ntTable->select(['brand', 'name', 'address', 'loc_lat', 'loc_lng', 'PDL'])->where(['PDL IS NOT NULL'])->orderAsc('PDL')->limit(3)->toArray();
+        }
+
         $allresults = json_encode(['Status'=>'00',
             'NSW'=>['U91'=>$nswCheapU91,'E10'=>$nswCheapE10,'P95'=>$nswCheapP95,'P98'=>$nswCheapP98,'DL'=>$nswCheapDL,'PDL'=>$nswCheapPDL,'LPG'=>$nswCheapLPG],
+            'NT'=>['U91'=>$NtCheapU91,'E10'=>$NtCheapE10,'P95'=>$NtCheapP95,'P98'=>$NtCheapP98,'DL'=>$NtCheapDL,'PDL'=>$NtCheapPDL,'LPG'=>$NtCheapLPG,'LAF'=>$NtCheapLAF],
             'TAS'=>['U91'=>$TasCheapU91,'E10'=>$TasCheapE10,'P95'=>$TasCheapP95,'P98'=>$TasCheapP98,'DL'=>$TasCheapDL,'PDL'=>$TasCheapPDL,'LPG'=>$TasCheapLPG],
             'WA'=>['U91'=>$WaCheapU91,'P95'=>$WaCheapP95,'P98'=>$WaCheapP98,'DL'=>$WaCheapDL,'PDL'=>$WaCheapPDL,'LPG'=>$WaCheapLPG]]);
 
